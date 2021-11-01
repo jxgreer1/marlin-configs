cc5e6eb450fba79055e4f4abc841924280ced53a
jxgreer1 <jackxgreer@icloud.com>
2021-11-01 09:22:22 -0700

    balls

--- filament sensro/Marlin-2.0.x/Marlin/Configuration.h
+++ filament sensro/Marlin-2.0.x/Marlin/Configuration.h
@@ -1435,6 +1435,7 @@
  *
  * RAMPS-based boards use SERVO3_PIN for the first runout sensor.
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
+ * set to high as test, need to check 
  */
 #define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
